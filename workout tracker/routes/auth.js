0d0f446a4d450fd48a585a35f4d84ca8da1bb78c
IljaRizhkov <cajkinigrok@gmail.com>
2026-09-13 12:29:31 +0300

    Changes

--- workout tracker/routes/auth.js
+++ workout tracker/routes/auth.js
@@ -71,5 +71,31 @@ router.post('/login', async (req, res) => {
 });
 
 
+//Logout User 
+router.post('/logout', async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne ({email});
+        if (!user) {
+            return res.status(400).json({message: 'User does not exist'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ id: user._id} , process.env.JWT_SECRET, {expiresIn: '1h' });
+        res.status(200).json({message: 'Logout successful'});
+
+    } catch (err) {
+        res.status(500).json({message: 'Server Error'});
+    }
+
+
+});
+
+
 module.exports = router;
 
